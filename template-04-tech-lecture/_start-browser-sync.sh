424ad9416093a1cf623fe3cbd884ee5560d06bfa
nab391 <nab@garegga.net>
2026-02-23 12:04:11 +0900

    template-04整理

--- template-04-tech-lecture/_start-browser-sync.sh
+++ template-04-tech-lecture/_start-browser-sync.sh
@@ -2,7 +2,7 @@
 
 # SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 # cd ${SCRIPT_DIR}/..
-cd dist
+# cd dist
 INDEX="${1:-slide.html}"
 browser-sync start --server --files "*.html" --index ${INDEX}
 
